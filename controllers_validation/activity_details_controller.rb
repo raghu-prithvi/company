0fef4a35a96a7c022206cc0d7743a1f06accae24
raghavendra <raghavendra.sangarsu@prithvisolutions.com>
2011-02-23 17:51:22 +0530

    change in the branch

--- controllers_validation/activity_details_controller.rb
+++ controllers_validation/activity_details_controller.rb
@@ -54,31 +54,7 @@ def create
 end
  
 
-def delete
-  user_email = params[:email]
-  start_date = params[:start_date]
-  end_date = params[:end_date]
-  @user_email_value = validate(user_email)
-  @start_date_value = validate(start_date)
-  @end_date_value = validate(end_date)
-  if(@user_email_value == 1 && @start_date_value ==1 && @end_date_value == 1)
-    @user = User.find_all_by_email(user_email)
-    if(!@user.blank?)
-      conditions = []
-      conditions.add_condition!(["updated_at>= ?",start_date])
-      conditions.add_condition!(["updated_at<= ?",end_date])
-      @activities_list = ActivityDetails.find_all_by_user_id(@user[0].id, :conditions => conditions)
-      @activities_list.each do |activity|
-      activity.destroy
-    end
-    else
-      render :text => "0"
-    end
-  else
-    render :text => "0"
-  end 
-    
-end
+raghu 
 
-end
-  
+testing 
+s
